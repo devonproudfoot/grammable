71ab6beee9ea9c033291105fd5f2452d34683224
Devon Proudfoot <proudfoot.devon@gmail.com>
2019-08-19 18:33:12 +0000

    Added new and create functionality for grams, with testing

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,6 +1,26 @@
 class GramsController < ApplicationController
 
   def index
+    @grams = Gram.all
+  end
+
+  def new
+    @gram = Gram.new
+  end
+
+  def create
+    @gram = Gram.create(gram_params)
+    if @gram.invalid?
+      return render :new, status: :unprocessable_entity
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def gram_params
+    params.require(:gram).permit(:message)
   end
 
 end
